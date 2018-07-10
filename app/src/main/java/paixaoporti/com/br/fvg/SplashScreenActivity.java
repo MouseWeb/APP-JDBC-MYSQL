59607417c5657cdb49cf3e921d7779354b4c59e3
Douglas Coelho <40206726+DouglasDeveloperApp@users.noreply.github.com>
2018-07-10 01:00:41 -0300

    .

--- app/src/main/java/paixaoporti/com/br/fvg/SplashScreenActivity.java
+++ app/src/main/java/paixaoporti/com/br/fvg/SplashScreenActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 600;
+    private static int SPLASH_TIME_OUT = 600;jvj
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
